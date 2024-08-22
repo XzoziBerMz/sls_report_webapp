2a052d949be48eb72155dada651837ce9670abe4
Arthitwed <arthit7877mc@gmail.com>
2024-08-22 15:50:37 +0700

    up

--- static/public/page-dashboard-new/js/index.js
+++ static/public/page-dashboard-new/js/index.js
@@ -1,64 +1,139 @@
-(function($, window, Vue, axios) {
+(function ($, window, Vue, axios) {
     'use strict';
-    
-   
+
+
 
     const app = Vue.createApp({
-        data: function() {
-            return{
-               user:window.user || "",
-               currentPage: window.currentPage,
-               authstatus:window.authstatus,
-               datas:[],
-               inventoryDetail:[],
-               search:"",
-               filtered:[],
+        data: function () {
+            return {
+                user: window.user || "",
+                currentPage: window.currentPage,
+                authstatus: window.authstatus,
+                datas: [],
+                inventoryDetail: [],
+                search: "",
+                filtered: [],
+                dataReview: [],
+                dataCaretaker: [],
+                dataClip: [],
+                dataProductChannel: [],
 
-                                  
             }
         },
         computed: {
-         
-      
-
 
         },
         methods: {
             async init() {
                 let self = this
-                             
+
             },
+            async loadDataClip() {
+                const self = this;
+                try {
+                    let data = {
+                        page: 1,
+                        per_page: 100,
+                    }
+                    let responseGetClip = await services.getClip();
+                    const dataClip = responseGetClip?.data.data || [];
+                    self.dataClip = dataClip;
 
-            getPokemon:async function (){
-              const self = this;
-              try {
-                  showLoading();
-                  const response = await services.getPokemon({})
-                  if (response){
-                      console.log(response)
-                      closeLoading();
+                } catch (error) {
+                    console.warn(`🌦️ ~ onClickSearch ~ error:`, error);
+                }
+            },
+            async loadDataProductChannel() {
+                const self = this;
+                try {
+                    let data = {
+                        page: 1,
+                        per_page: 100,
+                    }
+                    let responseGetProductChannel = await services.getProductChannel();
+                    const dataProductChannel = responseGetProductChannel?.data.data || [];
+                    self.dataProductChannel = dataProductChannel;
 
-                  }
-  
-              }catch(err){
-                  closeLoading();
-                  Msg("errorMsg",'error');
-                 
-              }finally{
-  
-              }
+                } catch (error) {
+                    console.warn(`🌦️ ~ onClickSearch ~ error:`, error);
+                }
+            },
+            async loadDataReview() {
+                const self = this;
+
+                try {
+                    let data = {
+                        page: 1,
+                        per_page: 100,
+                    }
+                    let responseGetAccount = await services.getReview();
+                    const dataReview = responseGetAccount?.data.data || [];
+                    self.dataReview = dataReview;
+
+                } catch (error) {
+                    console.warn(`🌦️ ~ onClickSearch ~ error:`, error);
+                }
+            },
+            async loadDataSelect() {
+                try {
+                    const responseGetCaretaker = await services.getCaretaker();
+                    const dataCaretaker = responseGetCaretaker?.data.data || [];
+                    this.dataCaretaker = dataCaretaker
+                    await this.initSelect();
+                } catch (error) {
+                    console.warn(`🌦️ ~ loadDataSelect ~ error:`, error);
+                }
+            },
+            initSelect() {
+                const self = this;
+
+                self.dataReview.forEach((list, index) => {
+                    const selected = '#select_admin_' + index
+                    $(selected).select2({
+                        minimumResultsForSearch: -1,
+                        allowClear: true,
+                        placeholder: 'Select Caretaker',
+                        data: self.dataCaretaker.map((item) => ({
+                            id: item.code,
+                            text: item.name
+                        }))
+                    })
+                });
             }
+            // getPokemon:async function (){
+            //   const self = this;
+            //   try {
+            //       showLoading();
+            //       const response = await services.getPokemon({})
+            //       if (response){
+            //           console.log(response)
+            //           closeLoading();
+
+            //       }
+
+            //   }catch(err){
+            //       closeLoading();
+            //       Msg("errorMsg",'error');
+
+            //   }finally{
+
+            //   }
+            // }
 
         },
 
         mounted: function () {
-           let self = this
-        //    self.getPokemon()
-           console.log("ok")
+            let self = this
+            //    self.getPokemon()
+            self.loadDataClip()
+            self.loadDataProductChannel()
+            self.loadDataReview()
+            self.loadDataSelect()
+            console.log("ok")
         }
 
-        
+
     });
-    
-const vue = app.mount("#kt_app_root");
+
+    const vue = app.mount("#kt_app_root");
 })(jQuery, window, Vue, axios);
--- static/public/page-dashboard-new/js/services.js
+++ static/public/page-dashboard-new/js/services.js
@@ -1,21 +1,68 @@
 
-(function($, window, axios) {
+(function ($, window, axios) {
+
     const endpoint = {
-         getPokemon:`/get-pokemon`,
-        
+        getReview: `/page-dashboard-new/mockjson/get_review.json`,
+
     }
-    
-    function getPokemon (body = {}) {
-        console.log( window.csrfToken)
-        return axios({
-            method: 'post',
-            url: endpoint.getPokemon,
-            data: {...body},
-            headers: {"X-Csrf-Token":window.csrfToken}
-        });
+
+    // function getPokemon (body = {}) {
+    //     console.log( window.csrfToken)
+    //     return axios({
+    //         method: 'post',
+    //         url: endpoint.getPokemon,
+    //         data: {...body},
+    //         headers: {"X-Csrf-Token":window.csrfToken}
+    //     });
+    // }
+
+
+    function getClip(body = {}) {
+        console.log(window.csrfToken)
+        return axios.get(`/page-dashboard-new/mockjson/get_clip.json`)
+        // return axios({
+        //     method: 'post',
+        //     url: endpoint.getReview,
+        //     data: { ...body },
+        //     headers: { "x-auth-token": window.csrfToken }
+        // });
     }
+    function getProductChannel(body = {}) {
+        console.log(window.csrfToken)
+        return axios.get(`/page-dashboard-new/mockjson/product_channel.json`)
+        // return axios({
+        //     method: 'post',
+        //     url: endpoint.getReview,
+        //     data: { ...body },
+        //     headers: { "x-auth-token": window.csrfToken }
+        // });
+    }
+    function getReview(body = {}) {
+        console.log(window.csrfToken)
+        return axios.get(`/page-dashboard-new/mockjson/get_review.json`)
+        // return axios({
+        //     method: 'post',
+        //     url: endpoint.getReview,
+        //     data: { ...body },
+        //     headers: { "x-auth-token": window.csrfToken }
+        // });
+    }
+    function getCaretaker(body = {}) {
+        console.log(window.csrfToken)
+        return axios.get(`/page-dashboard-new/mockjson/get_caretaker.json`)
+        // return axios({
+        //     method: 'post',
+        //     url: endpoint.getCaretaker,
+        //     data: { ...body },
+        //     headers: { "x-auth-token": window.csrfToken }
+        // });
+    }
+
     const services = {
-        getPokemon,
+        getReview,
+        getCaretaker,
+        getClip,
+        getProductChannel,
     };
     window.services = services;
 })(jQuery, window, axios);
